2232d11e87f75fcfffca7e518472819d228421d2
traustid@gmail.com <traustid@gmail.com>
2020-12-03 11:13:05 +0000

    base_name > basename

--- vefur_rest_api/urls.py
+++ vefur_rest_api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.documentation import include_docs_urls
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'pages', views.PagesViewSet, base_name='pages')
-router.register(r'front_sections', views.ForsiduhlutarViewSet, base_name='front_sections')
+router.register(r'pages', views.PagesViewSet, basename='pages')
+router.register(r'front_sections', views.ForsiduhlutarViewSet, basename='front_sections')
 
 urlpatterns = [
 	url(r'^', include(router.urls))
